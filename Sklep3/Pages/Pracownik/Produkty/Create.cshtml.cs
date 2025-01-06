8ccba586b4b3a178eed1ac48ea5f0e861c2a1f0c
Kass-i <kasia.idasz@gmail.com>
2025-01-06 18:39:57 +0100

    Wyczyszczono kod

--- Sklep3/Pages/Pracownik/Produkty/Create.cshtml.cs
+++ Sklep3/Pages/Pracownik/Produkty/Create.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
+using Sklep3.Pages.Shared;
 
 namespace Sklep3.Pages.Pracownik.Produkty
 {
